e02a638cebd62b2195d363df1cbad99ed374e072
CoderDi <1138832931@qq.com>
2021-12-07 10:51:35 +0800

    init: create-react-app

---
+++ .eslintrc.js
@@ -0,0 +1,61 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'airbnb',
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 13,
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+  ],
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-unused-vars': ['warn',
+      { vars: 'all', args: 'after-used', ignoreRestSiblings: false },
+    ],
+    'no-param-reassign': [
+      'error',
+      {
+        props: true,
+      },
+    ],
+    'import/no-unresolved': 'off',
+    indent: ['error', 2, { SwitchCase: 1 }],
+    quotes: ['error', 'single'],
+    semi: ['error', 'always'],
+    'comma-dangle': ['error', 'only-multiline'],
+    'max-len': [
+      'error',
+      {
+        code: 80,
+        tabWidth: 2,
+        ignoreComments: true,
+        ignoreTrailingComments: true,
+        ignoreUrls: true,
+        ignoreStrings: true,
+        ignoreTemplateLiterals: true,
+        ignoreRegExpLiterals: true,
+      },
+    ],
+    'jsx-quotes': [2, 'prefer-single'],
+    'react/function-component-definition': [
+      2,
+      {
+        namedComponents: 'arrow-function',
+        unnamedComponents: 'arrow-function',
+      },
+    ],
+  },
+};
---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import '@/App.less';
+
+const App = () => (
+  <div className='app'>
+    <h1>Hello World</h1>
+  </div>
+);
+
+export default App;
