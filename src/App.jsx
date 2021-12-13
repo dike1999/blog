df086dd16a1fec82e67099f69189128aff94fa95
CoderDi <1138832931@qq.com>
2021-12-13 18:07:09 +0800

    feat: config.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import lazy from '@/components/Lazy';
 
 const Components = lazy(() => import('@/components/404'));
-
 const App = () => (
   <div className='app'>
     <Components />
