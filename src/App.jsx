2b789d8af250d170dc24ccaaf50a1bea2c29d691
CoderDi <1138832931@qq.com>
2021-12-13 22:31:20 +0800

    refactor: App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,72 @@
+/* eslint-disable react/no-unstable-nested-components */
+/* eslint-disable no-param-reassign */
 import React from 'react';
-import lazy from '@/components/Lazy';
+import {
+  BrowserRouter, Route, Switch, Redirect
+} from 'react-router-dom';
+import { useSelector } from 'react-redux';
 
-const Components = lazy(() => import('@/components/MyInfo'));
-const App = () => (
-  <div className='app'>
-    <Components />
-  </div>
-);
+import routesData from '@/routes';
+import PublicComponent from '@/components/Public';
+
+const App = () => {
+  const role = useSelector((state) => state.user.role);
+
+  // 解构 route
+  const renderRoutes = (routes, contextPath) => {
+    const children = [];
+
+    const renderRoute = (item, routeContextPath) => {
+      let newContextPath = item.path
+        ? `${routeContextPath}/${item.path}`
+        : routeContextPath;
+      newContextPath = newContextPath.replace(/\/+/g, '/');
+      if (newContextPath.includes('admin') && role !== 1) {
+        item = {
+          ...item,
+          component: () => <Redirect to='/' />,
+          children: [],
+        };
+      }
+      if (!item.component) return;
+
+      if (item.childRoutes) {
+        const childRoutes = renderRoutes(item.childRoutes, newContextPath);
+        children.push(
+          <Route
+            key={newContextPath}
+            render={(props) => (
+              <item.component {...props}>{childRoutes}</item.component>
+            )}
+            path={newContextPath}
+          />
+        );
+        item.childRoutes.forEach((r) => renderRoute(r, newContextPath));
+      } else {
+        children.push(
+          <Route
+            key={newContextPath}
+            component={item.component}
+            path={newContextPath}
+            exact
+          />
+        );
+      }
+    };
+
+    routes.forEach((item) => renderRoute(item, contextPath));
+
+    return <Switch>{children}</Switch>;
+  };
+
+  return (
+    <div className='app'>
+      <BrowserRouter>
+        {renderRoutes(routesData, '/')}
+        <PublicComponent />
+      </BrowserRouter>
+    </div>
+  );
+};
 
 export default App;
