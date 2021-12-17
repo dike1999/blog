9df9689e2b9080f4236466450f14f1a0b2d2eb70
dike1999 <1138832931@qq.com>
2021-12-17 16:08:45 +0800

    feat: add deploy.sh

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,7 @@ import viteCompression from 'vite-plugin-compression';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/blog-vite/',
   plugins: [
     react(),
     styleImport({
